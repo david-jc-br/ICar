5bf7c58e9c8287bbb20283e8e6ca62830e760cf3
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2023-07-21 17:12:03 -0300

    Testes unitários OK !

---
+++ App/Back-end/src/tests/unit/veiculoValidacao.spec.js
@@ -0,0 +1,53 @@
+const { expect } = require('chai');
+const {
+    ehValidaCor,
+    ehValidaDisponibilidade,
+    ehValidoAno,
+    ehValidoCombustivel,
+    ehValidoMarca,
+    ehValidoModelo,
+    ehValidoPlaca,
+    ehValidoValorDiaria,
+} = require('../../services/veiculos.services');
+
+describe('Testes Unitários para as funções de validação', () => {
+    it('Deve lançar um erro para cor inválida', () => {
+        const invalidColor = 'Roxo';
+        expect(() => ehValidaCor(invalidColor)).to.throw('A cor selecionada não é válida.');
+    });
+
+    it('Deve lançar um erro para disponibilidade inválida', () => {
+        const invalidDisponibilidade = 'Indisponível';
+        expect(() => ehValidaDisponibilidade(invalidDisponibilidade)).to.throw('A disponibilidade selecionada não é válida.');
+    });
+
+    it('Deve lançar um erro para ano inválido', () => {
+        const invalidAno = 2021;
+        expect(() => ehValidoAno(invalidAno)).to.throw('O ano deve estar entre 2022 e 2024.');
+    });
+
+    it('Deve lançar um erro para combustível inválido', () => {
+        const invalidCombustivel = 'Diesel';
+        expect(() => ehValidoCombustivel(invalidCombustivel)).to.throw('O combustível selecionado não é válido.');
+    });
+
+    it('Deve lançar um erro para marca inválida', () => {
+        const invalidMarca = 'A';
+        expect(() => ehValidoMarca(invalidMarca)).to.throw('A marca deve ter entre 2 e 100 caracteres.');
+    });
+
+    it('Deve lançar um erro para modelo inválido', () => {
+        const invalidModelo = 'X';
+        expect(() => ehValidoModelo(invalidModelo)).to.throw('O modelo deve ter entre 2 e 100 caracteres.');
+    });
+
+    it('Deve lançar um erro para placa inválida', () => {
+        const invalidPlaca = 'ABCY1234';
+        expect(() => ehValidoPlaca(invalidPlaca)).to.throw('A placa deve ter exatamente 7 caracteres.');
+    });
+
+    it('Deve lançar um erro para valor diária inválido', () => {
+        const invalidValorDiaria = 80.0;
+        expect(() => ehValidoValorDiaria(invalidValorDiaria)).to.throw('O valor da diária deve ser igual ou superior a 90.00 reais.');
+    });
+});
